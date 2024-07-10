fb543c4083cef1acd502a00c9d5ed16a20a5899b
Hritik Gupta <52317704+Hritik-Board@users.noreply.github.com>
2024-07-10 15:08:01 +0530

    Event Handlers

---
+++ JavaScript/DOM/DOM Event Handling/InlineEvent handlers/script.js
@@ -0,0 +1,62 @@
+//mouse event
+
+//click event
+
+function clickhandler() {
+  console.log("Click has beeen performed");
+}
+
+//dbl click
+function doublehandler() {
+  console.log("double Click has beeen performed");
+}
+
+//mouseoverhandler
+function mouseoverhandler() {
+  console.log("mouseoverhandler has beeen performed");
+}
+
+//mouseouthandler
+function mouseouthandler() {
+  console.log("mouseouthandler has beeen performed");
+}
+
+//keyboard event
+
+//keydownhandler event
+
+function keydownhandler() {
+  console.log("keydownhandler has beeen performed");
+}
+
+//keyuphandler event
+
+function keyuphandler() {
+  console.log("keyuphandler has beeen performed");
+}
+
+//keypresshandler event
+
+function keypresshandler() {
+  console.log("keypresshandler has beeen performed");
+}
+
+//form  event
+
+//onsubmithandler event
+
+function onsubmithandler() {
+  console.log("onsubmithandler has beeen performed");
+}
+
+//onfocushandler event
+
+function onfocushandler() {
+  console.log("onfocushandler has beeen performed");
+}
+
+//onblurhandler event
+
+function onblurhandler() {
+  console.log("onblurhandler has beeen performed");
+}
---
+++ JavaScript/DOM/DOM Event Handling/Traditional Event Handlers/script.js
@@ -0,0 +1,72 @@
+//mouse event
+
+//click event
+
+document.getElementById("clickButton").onclick = function clickButton() {
+  console.log("clicked");
+};
+
+// clickButtonEle
+
+// console.log(clickButtonEle);
+
+// function clickhandler() {
+//   // console.log("Click has beeen performed");
+// }
+
+//dbl click
+
+document.getElementById("doubleClickButton").onclick =
+  function doublehandler() {
+    console.log("double Click has beeen performed");
+  };
+
+// //mouseoverhandler
+// function mouseoverhandler() {
+//   console.log("mouseoverhandler has beeen performed");
+// }
+
+// //mouseouthandler
+// function mouseouthandler() {
+//   console.log("mouseouthandler has beeen performed");
+// }
+
+// //keyboard event
+
+// //keydownhandler event
+
+// function keydownhandler() {
+//   console.log("keydownhandler has beeen performed");
+// }
+
+// //keyuphandler event
+
+// function keyuphandler() {
+//   console.log("keyuphandler has beeen performed");
+// }
+
+// //keypresshandler event
+
+// function keypresshandler() {
+//   console.log("keypresshandler has beeen performed");
+// }
+
+// //form  event
+
+// //onsubmithandler event
+
+// function onsubmithandler() {
+//   console.log("onsubmithandler has beeen performed");
+// }
+
+// //onfocushandler event
+
+// function onfocushandler() {
+//   console.log("onfocushandler has beeen performed");
+// }
+
+// //onblurhandler event
+
+// function onblurhandler() {
+//   console.log("onblurhandler has beeen performed");
+// }
